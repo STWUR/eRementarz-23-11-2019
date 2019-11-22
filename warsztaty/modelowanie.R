d6020cc4f47a48002fdd08272d002d5c1883fef4
Dominik Rafacz <d.rafacz@student.mini.pw.edu.pl>
2019-11-22 16:09:06 +0100

    prezentacja i warsztaty modelowanie

---
+++ warsztaty/modelowanie.R
@@ -0,0 +1,13 @@
+## 1. wczytywanie zbioru heart.csv
+
+## 2. prosta eksploracja i wizualizacja
+
+## 3. utworzenie zadania klasyfikacyjnego i modeli
+
+## 4. trenowanie modeli
+
+## 5. predykcja
+
+## 6. wizualizacja modeli
+
+## 7. walidacja krzyżowa
